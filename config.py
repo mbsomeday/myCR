e781d028b65a6c8c012a97d96a13510ae45401c9
villager_wang <137542994@qq.com>
2021-07-19 18:05:38 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,58 @@
+import argparse
+import multiprocessing
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--width', type=int, default=200)
+parser.add_argument('--height', type=int, default=32)
+parser.add_argument('--nb_channels', type=int, default=3)
+parser.add_argument('--label_len', type=int, default=7)   # 最长的标签长度
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--model', type=str, default='CRNN_STN', choices=['CRNN_CTN', 'CRNN'])
+parser.add_argument('--conv_filter_size', type=int, nargs=7, default=[64, 128, 256, 256, 512, 512, 512])
+parser.add_argument('--lstm_nb_units', type=int, nargs=2, default=[128, 128])
+parser.add_argument('--timesteps', type=int, default=50)
+parser.add_argument('--dropout_rate', type=float, default=0.25)
+parser.add_argument('--optimizer', type=str, default='sgd')
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--lr_reduction_factor', type=float, default=0.1)
+parser.add_argument('--nb_epochs', type=int, default=1)
+parser.add_argument('--nb_workers', type=int, default=multiprocessing.cpu_count())
+parser.add_argument('--resume_training', type=bool, default=False)
+
+parser.add_argument('--characters', type=str, default='0123456789-')
+
+parser.add_argument('--base_dir', type=str, default=r'./tianchi')
+parser.add_argument('--output_dir', type=str, default=r'./tianchi_result')
+parser.add_argument('--save_best_only', type=bool, default=True)
+parser.add_argument('--training_model_cp_filename', type=str, default=r'./model/model.{epoch:03d}-{loss:.2f}.h5')
+parser.add_argument('--prediction_model_cp_filename', type=str, default=r'./model/prediction_model.{epoch:03d}.h5')
+parser.add_argument('--load_model_path', type=str, default='')
+parser.add_argument('--tb_lob', type=str, default='tb_log')
+
+cfg = parser.parse_args()
+
+
+if __name__ == '__main__':
+    print(type(cfg))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
